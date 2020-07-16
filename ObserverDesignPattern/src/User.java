9033acb2e6e96d523ea76d39b78f86f247ee985f
Ashis Chakraborty <43085234+ashis041@users.noreply.github.com>
2020-07-16 11:24:48 +0600

    Update User.java

--- ObserverDesignPattern/src/User.java
+++ ObserverDesignPattern/src/User.java
@@ -3,17 +3,17 @@ class User implements Observer {
     
 	private String name;
     
-    User(String name){
+        User(String name){
 		this.name = name;
 	}
 	
 	public void Subscribe(Group grp) {
-		grp.registerObserver(this);
+		grp.registerObserver(this); //register as observer to group
 		System.out.print("\n"+this.name+" subscribes "+grp.getName()+"\n");
 	}
 	
 	public void unSubscribe(Group grp) {
-		grp.removeObserver(this);
+		grp.removeObserver(this);  //unsubscribe from Group class
 		System.out.print("\n"+this.name+" unsubscribes "+grp.getName()+"\n");
 	}
 	
